0f34a1238c24f51009c0825b42fe7ce5738ae55a
himkumar_kuliza <himanshu.kumar@kuliza.com>
2019-07-26 21:42:10 +0530

    react-redux boiler plate

---
+++ src/components/postform.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createPosts } from "../actions/postActions";
+ 
+class Postform extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: '',
+			body: ''
+		};
+	}
+
+	onChange = (e) => {
+		this.setState({[e.target.name]:e.target.value});
+	}
+
+	onSubmit = (e)=>{
+		e.preventDefault();
+
+		const post = {
+			title: this.state.title,
+			body: this.state.body
+		};
+		this.props.createPosts(post);
+	}
+
+	render() {
+		return (
+			<>
+				<h1>Add Posts</h1>
+				<form onSubmit={this.onSubmit}>
+					<div>
+						<label>Title</label><br />
+						<input type="text" name="title" value={this.state.title} onChange={this.onChange} />
+					</div>
+					<br />
+					<div>
+						<label>Body</label><br />
+						<textarea type="text" rows="3" name="body" value={this.state.body} onChange={this.onChange} />
+					</div>
+					<br />
+					<button type="submit">Submit</button>
+				</form>
+			</>
+		)
+	}
+}
+
+Postform.propTypes = {
+	createPosts: PropTypes.func.isRequired
+}; 
+
+export default connect(null, { createPosts})(Postform);
